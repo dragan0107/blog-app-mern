c5f2fe9c4d0557381e264d2e7b7fdb4ad5ba3cf2
drip016 <73792907+drip016@users.noreply.github.com>
2021-11-02 01:57:21 +0100

    single post, settings and write components

---
+++ src/pages/settings/Settings.jsx
@@ -0,0 +1,33 @@
+import './settings.css'
+import Sidebar from "../../components/sidebar/Sidebar"
+
+export default function Settings() {
+    return (
+        <div className="settings">
+            <div className="settingsWrapper">
+                <div className="settingsTitle">
+                    <span className="settingsUpdateTitle">Update your account.</span>
+                    <span className="settingsDeleteTitle">Delete account.</span>
+                </div>
+                <form  className="settingsForm">
+                    <label>Profile Picture.</label>
+                    <div className="settingsPfP">
+                        <img src="https://images.unsplash.com/photo-1546587348-d12660c30c50?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJhbHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&w=1000&q=80" alt="" />
+                        <label htmlFor="fileInput">
+                            <i className="settingsPfPIcon far fa-user-circle"></i>
+                        </label>
+                        <input type="file" id="fileInput" style={{display: 'none'}}/>
+                    </div>
+                    <label>Username</label>
+                    <input type="text" placeholder="Drip"/>
+                    <label>Email</label>
+                    <input type="email" placeholder="drip@gmail.com"/>
+                    <label>Password</label>
+                    <input type="password"/>
+                    <button className="settingsSubmit">Update.</button>
+                </form>
+            </div>
+            <Sidebar/>
+        </div>
+    )
+}
---
+++ src/pages/write/Write.jsx
@@ -0,0 +1,22 @@
+import './write.css'
+
+export default function Write() {
+    return (
+        <div className="write">
+            <form className="writeForm">
+            <img className="writeImg" src="https://preview.redd.it/491dibzljr121.jpg?auto=webp&s=cab73925a12959dbc71a1977dc0aa86447ac432a" alt="" />
+                <div className="writeFormGroup">
+                    <label htmlFor="fileInput">
+                        <i class="writeIcon fas fa-plus"></i>
+                    </label>
+                    <input type="file" id="fileInput" style={{display: "none"}}/>
+                    <input type="text" placeholder="Title" className="writeInput" autoFocus={true}/>
+                </div>
+                <div className="writeFormGroup">
+                    <textarea type="text" placeholder="Tell us your story.." className="writeInput writeText"></textarea>
+                </div>
+                <button className="writeSubmit">Publish!</button>
+            </form>
+        </div>
+    )
+}
